f432f1c4574af77b10540b9b21cfa24d6994c60b
nyem <1857991+nyem69@users.noreply.github.com>
2023-01-24 22:06:37 +0800

    sync

--- src/routes/harga/peringkat/+page.svelte
+++ src/routes/harga/peringkat/+page.svelte
@@ -13,7 +13,7 @@
 
 	iframe {
 		width:100%;
-		min-height:700px;
+		min-height:10000px;
 		border:none;
 		margin-top:32px;
 		background:#fff;
@@ -25,6 +25,6 @@
 <Header />
 
 <!--
-<iframe title="dashboard" src="https://harga.kb.asia-southeast1.gcp.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/a52fa4c0-9a69-11ed-a0f6-bfe9bbfa0f66?embed=true&_g=!n&hide-filter-bar=false" width="100%" height="7000px">></iframe>
+<iframe title="dashboard" src="https://harga.kb.asia-southeast1.gcp.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/a52fa4c0-9a69-11ed-a0f6-bfe9bbfa0f66?embed=true&_g=!n&hide-filter-bar=false" width="100%" height="10000px">></iframe>
 -->
-<iframe title="dashboard" src="https://harga.kb.asia-southeast1.gcp.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/a52fa4c0-9a69-11ed-a0f6-bfe9bbfa0f66?embed=true&_g=!n&show-query-input=true" width="100%" height="7000px"></iframe>
+<iframe title="dashboard" src="https://harga.kb.asia-southeast1.gcp.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/a52fa4c0-9a69-11ed-a0f6-bfe9bbfa0f66?embed=true&_g=!n&show-query-input=true" width="100%" height="10000px"></iframe>
