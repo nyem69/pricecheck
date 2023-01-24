cd4adcd50986442b72751dbb8caec1713e01425d
nyem <1857991+nyem69@users.noreply.github.com>
2023-01-24 13:52:04 +0800

    Build output: /docs
    Build output directory:
    /docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		//adapter: adapter()
+		adapter: adapter({ pages: 'docs', assets: 'docs', fallback: 'app.html', precompress: true }),
 	}
 };
 
 export default config;
+
