d37bd9eea486fcf8038e77751adf7026a6e448e2
nyem <1857991+nyem69@users.noreply.github.com>
2023-01-25 13:21:36 +0800

    select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,7 @@
 	import { writable } from 'svelte/store';
 
 	import * as d3 from "d3"
+	import d3comparator from '$lib/d3comparator.js';
 
 	let dbg=1;
 	let el;
@@ -29,9 +30,9 @@
 
 //			L = await import('leaflet');
 
+//				d3.select(el).call(init);
 
 //			console.log('lookup_item', lookup_item);
-			d3.select(el).call(init);
 
 
 			const savedList = window.localStorage.getItem('savedList') || null;
@@ -64,7 +65,14 @@
 
 
 
+
+	//====================================================================================
+	//
+	//====================================================================================
 	function subtree(sel, key)	{
+
+		dbg&&console.group('%c'+'subtree-'+key, 'color:magenta');
+		dbg&&console.log('key', key);
 
 		sel.selectChildren('li').data(d=>d)
 			.join('li').attr('class','bp4-tree-node bp4-tree-node-expanded')
@@ -92,16 +100,26 @@
 
 
 					sel.selectChildren('ul').data(d=>{
-						let k = d3.groups(d[1], d=>d[key]);
-						console.log(key, k)
-						return [k];
+						if (key=='item_category')	{
+							let k = d3.groups(d[1], d=>d[key]);
+							console.log(key, k)
+							return [k];
+						}else if (key=='item_subcategory')	{
+							let k = d3.groups(d[1], d=>d[key]);
+							console.log(key, k)
+							return [k];
+						}else	{
+							return []
+						}
 					})
 						.join('ul').attr('class','bp4-tree-node-list')
 							.style('margin-left','32px')
 							.call(sel=>{
 
 								if (key=='item_category')	{
-									sel.call(subtree, key=='item_subcategory')
+									sel.call(subtree, 'item_subcategory')
+								}else	{
+									//sel.call(subtree, 'item')
 								}
 
 
@@ -109,10 +127,187 @@
 
 				});
 
+		dbg&&console.groupEnd('subtree-'+key);
+
 	}
 
 
+	//====================================================================================
+	//
+	//====================================================================================
+
+	let menuList = d3.groups(data.lookup_item, d=>d.item_group)
+							.map(d=>{
+
+								return {
+									key			: d[0],
+									total		: d[1].length,
+									values	: d3.groups(d[1], d=>d['item_category'])
+															.map(d=>{
+
+																return {
+																	key			: d[0],
+																	total		: d[1].length,
+																	values	: d3.groups(d[1], d=>d['item_subcategory']||d['item_category'])
+																							.map(d=>{
+
+																								return {
+																									key			: d[0],
+																									total		: d[1].length,
+																									values	: d[1].sort(d3comparator().order(d3.ascending, d=>d['item']))
+																								}
+
+																							})
+																							.sort(d3comparator().order(d3.ascending, d=>d.key))
+																}
+
+															})
+															.sort(d3comparator().order(d3.ascending, d=>d.key))
+
+								}
+
+
+							})
+							.sort(d3comparator().order(d3.ascending, d=>d.key))
+
+	console.log('menuList', menuList);
+
+
+	//====================================================================================
+	//
+	//====================================================================================
+
+	function toggleTree()	{
+		console.group('%c'+'toggleTree','color:lime')
+		console.log(this);
+
+		let sel = d3.select(this),
+				parent = d3.select(this.parentNode),
+				expanded = !parent.classed('bp4-tree-node-expanded');
+
+		console.log('parent', parent.node());
+		console.log('expanded', expanded);
+
+		parent.classed('bp4-tree-node-expanded', expanded);
+		parent.select('.bp4-tree-node-list').style('display', expanded?null:'none');
+
+		sel.select('.caret')
+			.classed('bp4-tree-node-caret-open', expanded)
+//			.classed('bp4-icon-folder-open', expanded)
+
+		sel.select('.bp4-tree-node-icon')
+			.classed('bp4-icon-folder-close', !expanded)
+			.classed('bp4-icon-folder-open', expanded)
+
+		console.groupEnd('toggleTree')
+	}
+
+
+
+	//====================================================================================
+	//
+	//====================================================================================
+	function toggleItem()	{
+		console.group('%c'+'toggleItem','color:lime')
+		console.log(this);
+
+		let sel 		= d3.select(this),
+				active 	= !sel.classed('bp4-icon-tick-circle');
+
+		console.log('sel', sel.node());
+		console.log('active', active);
+
+		let nodeIcon = sel.select('.node-icon ')
+			//.classed('bp4-active', active)
+			.classed('bp4-icon-add', !active)
+			.classed('bp4-icon-tick-circle', active)
+
+		console.log('nodeIcon', nodeIcon.node());
+
+		console.groupEnd('toggleItem')
+	}
+
 </script>
+
+
+<div class="bp4-tree bp4-elevation-0">
+  <ul class="bp4-tree-node-list bp4-tree-root">
+
+  	{#each menuList as item_group}
+
+	    <li class="bp4-tree-node">
+
+				<!--
+	      	on:click={(e,d) => toggleTree(e,d)}
+	      -->
+
+	      <div class="bp4-tree-node-content"
+	      	on:click={toggleTree}
+	      	style="cursor:pointer"
+	      >
+	        <span class="caret bp4-tree-node-caret bp4-tree-node-caret-open bp4-icon-standard"></span>
+	        <span class="bp4-tree-node-icon bp4-icon-standard bp4-icon-folder-close"></span>
+	        <span class="bp4-tree-node-label">{item_group.key}</span>
+	        <span class="bp4-tree-node-secondary-label">{item_group.total}</span>
+	      </div>
+
+	      <ul class="bp4-tree-node-list" style="margin-left:24px; display:none">
+
+	      	{#each item_group.values as item_category}
+
+		        <li class="bp4-tree-node">
+		          <div class="bp4-tree-node-content" on:click={toggleTree} style="cursor:pointer">
+		            <span class="caret bp4-tree-node-caret bp4-icon-standard"></span>
+		            <span class="bp4-tree-node-icon bp4-icon-standard bp4-icon-folder-close"></span>
+		          	<span class="bp4-tree-node-label">{item_category.key}</span>
+		          	<span class="bp4-tree-node-secondary-label">{item_category.total}</span>
+		          </div>
+
+
+				      <ul class="bp4-tree-node-list" style="margin-left:24px; display:none">
+
+				      	{#each item_category.values as item_subcategory}
+
+					        <li class="bp4-tree-node">
+					          <div class="bp4-tree-node-content" on:click={toggleTree} style="cursor:pointer">
+					            <span class="caret bp4-tree-node-caret bp4-icon-standard"></span>
+					            <span class="bp4-tree-node-icon bp4-icon-standard bp4-icon-folder-close"></span>
+					          	<span class="bp4-tree-node-label">{item_subcategory.key}</span>
+					          	<span class="bp4-tree-node-secondary-label">{item_subcategory.total}</span>
+					          </div>
+
+
+					          <ul class="bp4-tree-node-list" style="margin-left:24px; display:none">
+
+							      	{#each item_subcategory.values as item}
+
+								        <li class="bp4-tree-node">
+								          <div class="item bp4-tree-node-content" on:click={toggleItem} style="cursor:pointer">
+								            <span class="bp4-tree-node-caret-none bp4-icon-standard"></span>
+								            <span class="node-icon bp4-tree-node-icon bp4-icon-standard bp4-icon-add"></span>
+								          	<span class="bp4-tree-node-label">{item.item}</span>
+								          </div>
+
+											{/each}
+
+										</ul>
+									</li>
+
+								{/each}
+
+							</ul>
+		        </li>
+
+	        {/each}
+
+	      </ul>
+	    </li>
+
+		{/each}
+
+  </ul>
+</div>
+
 
 
 <div bind:this={el}/>
